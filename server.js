3a681a2db13f7d9e034374ffc15bd9f33ca0e555
preyapanngam2004-art <preyapanngam2004@gmail.com>
2025-11-01 15:47:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -31,11 +31,9 @@ const upload = multer({ storage: storage });
 const dbConfig = {
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
-    // ... (ตัวแปรอื่นๆ)
-    port: process.env.MYSQLPORT,
-    
-    // --- ‼️ ต้องเพิ่มบรรทัดนี้เข้าไปครับ ‼️ ---
-    ssl: { "rejectUnauthorized": true } 
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 };
 
 async function getConnection() {
